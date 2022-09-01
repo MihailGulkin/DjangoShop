465e5473c62b5e99a3fe3cc00c14ccbb0090a0f5
Bol4onok <Bol4onok@example.com>
2022-09-01 23:34:57 +0300

    34 commit, refactoring

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -118,9 +118,12 @@ USE_THOUSAND_SEPARATOR = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'shop/static/media')
 MEDIA_URL = 'media/'
+
 LOGOUT_REDIRECT_URL = 'main_page'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
